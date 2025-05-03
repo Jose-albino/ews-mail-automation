3ac21e0f341d3932df636bf96fba591f58b3748e
ALBINO José <ALBINJO@int.consilium.europa.eu>
2025-05-03 21:00:34 +0200

    pytest fis
    fixing package setup pytest

--- setup.py
+++ setup.py
@@ -6,15 +6,5 @@ setup(
     description="A CLI-enabled Python package",
     author="Jose Albino",
     author_email="pypi-packages-email.pendant519@passmail.com",
-    packages=find_packages(),
-    install_requires=[],
-    entry_points={
-        "console_scripts": [
-            "exchange_automator=exchange_automator.cli:main"
-        ]
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-    ],
+    packages=find_packages()
 )
