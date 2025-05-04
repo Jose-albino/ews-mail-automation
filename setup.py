38f4f6f3c27789bef67054024effceb36fa231f0
jose albino <jose.albino@protonmail.com>
2025-05-04 23:14:49 +0200

    moved all info to toml

--- setup.py
+++ setup.py
@@ -1,10 +1,4 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="exchange_automator",
-    version="0.1.0",
-    description="A CLI-enabled Python package",
-    author="Jose Albino",
-    author_email="pypi-packages-email.pendant519@passmail.com",
-    packages=find_packages()
 )
