86f915967fa7b41ae6b675b4f44d9bac63992e9c
jose albino <jose.albino@protonmail.com>
2025-05-06 23:04:35 +0200

    adapting text

--- src/exchange_automator/cli.py
+++ src/exchange_automator/cli.py
@@ -2,11 +2,12 @@ import argparse
 from exchange_automator import __version__
 
 def main():
-    parser = argparse.ArgumentParser(description="My Package CLI")
+    description="Exchange Automator"
+    parser = argparse.ArgumentParser(description=description)
     parser.add_argument("-v", "--version", action="store_true", help="Show version")
     args = parser.parse_args()
 
     if args.version:
-        print(f"My Package version {__version__}")
+        print(f"Version is {__version__}")
     else:
-        print("Hello from my_package!")
+        print(description)
