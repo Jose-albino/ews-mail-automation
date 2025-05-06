5d37c57f745ea71ab1e3fa234175463150602f25
jose albino <jose.albino@protonmail.com>
2025-05-06 23:17:23 +0200

    fixing black formatting

--- src/exchange_automator/__init__.py
+++ src/exchange_automator/__init__.py
@@ -1,1 +1,4 @@
-from exchange_automator.__version import __version__  # <- dynamically injected by versioningit
+from exchange_automator.__version import (
+    __version__,
+    #  dynamically injected by versioningit
+)
